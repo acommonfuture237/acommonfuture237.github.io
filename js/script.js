8999983689028fb55e046f45f23d72cb61f7dd13
acommonfuture237 <acommonfuture237@yahoo.com>
2026-02-01 13:24:10 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
 function toggleMenu() {
-  document.getElementById("nav").classList.toggle("active");
+  document.getElementById("nav-menu").classList.toggle("show");
 }
-
-/* =========================
-   VISIBLE CONTENT ANIMATION
-   ========================= */
-
-window.addEventListener("load", () => {
-  document.querySelectorAll(".animate").forEach(el => {
-    // force initial state
-    el.classList.remove("show");
-
-    // visible delay so animation can be seen
-    setTimeout(() => {
-      el.classList.add("show");
-    }, 400);
-  });
-});
